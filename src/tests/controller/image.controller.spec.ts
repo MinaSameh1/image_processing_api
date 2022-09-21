9262493acb968dc21b99061b336aa621d9475abe
MinaSameh1 <minasameh1@gmail.com>
2022-09-21 23:22:31 +0200

    finished Controller spec

---
+++ src/tests/controller/image.controller.spec.ts
@@ -0,0 +1,28 @@
+import request from 'supertest'
+import app from '../../server'
+
+describe('Main Endpoint', () => {
+  beforeAll(() => {
+    process.env.LOG_LEVEL = 'silent'
+  })
+
+  it('Should return 400 on bad request', async () => {
+    await request(app).get('/api/img').expect(400)
+  })
+
+  it('Should return 404 on no image found', async () => {
+    await request(app).get('/api/img?filename=NOTHING').expect(404)
+  })
+
+  it('Should return 200 on img request', async () => {
+    await request(app).get('/api/img?filename=img.jpg').expect(200)
+  })
+
+  it('Should return 200 on img request with width and height', async () => {
+    await request(app).get('/api/img?filename=img.jpg&h=100&w=200').expect(200)
+  })
+
+  it('Should return 200 on img request with width only', async () => {
+    await request(app).get('/api/img?filename=img.jpg&w=200').expect(200)
+  })
+})
