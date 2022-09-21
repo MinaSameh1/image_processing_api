cb81709dfa37b0f7637d7261801ec70b69d6d2ff
MinaSameh1 <minasameh1@gmail.com>
2022-09-21 21:27:20 +0200

    misc

--- src/controller/server.controller.ts
+++ src/controller/server.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 
 /**
- * Endpoint to check that the server is running.
+ * @description Endpoint to check that the server is running.
  */
 export function checkHealthController(_: Request, res: Response) {
   return res.status(200).json({ message: 'Pong!' })
